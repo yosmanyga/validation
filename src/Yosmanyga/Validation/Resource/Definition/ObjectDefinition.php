c7a2b15b3abbb3523eccbfebb2141321aafbe144
Yosmany Garcia <yosmanyga@gmail.com>
2013-12-09 15:15:37 -0500

    Added self validator

--- src/Yosmanyga/Validation/Resource/Definition/ObjectDefinition.php
+++ src/Yosmanyga/Validation/Resource/Definition/ObjectDefinition.php
@@ -3,9 +3,24 @@
 namespace Yosmanyga\Validation\Resource\Definition;
 
 use Yosmanyga\Resource\Definition\Definition;
+use Yosmanyga\Validation\Validator\ValidatedInterface;
+use Yosmanyga\Validation\Validator\ObjectValidator;
+use Yosmanyga\Validation\Validator\ValueValidator;
 
-class ObjectDefinition extends Definition
+class ObjectDefinition extends Definition implements ValidatedInterface
 {
     public $class;
     public $validators;
+
+    /**
+     * @inheritdoc
+     */
+    public function createValidator()
+    {
+        return new ObjectValidator(array(
+            'class' => new ValueValidator(array(
+                'type' => 'string'
+            )),
+        ));
+    }
 }
